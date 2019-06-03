157ffcc84074d91123db035552fe14407d0b0388
Nguyễn Hữu Thạch <thachlongfv114@gmail.com>
2019-06-03 08:10:35 +0700

    Ve Phim

---
+++ app/src/main/java/com/congthuong/datvexemphim/intr_thucung.java
@@ -0,0 +1,13 @@
+package com.congthuong.datvexemphim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intr_thucung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intr_thucung);
+    }
+}
